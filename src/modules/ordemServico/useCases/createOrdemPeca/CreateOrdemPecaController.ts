7859ca122a7138cc6dd9a88639e74eec35994feb
NathanFelipe99 <nathanfguerlando@gmail.com>
2022-04-08 19:22:02 -0300

    fixing CreateOrdemPeca filename

--- src/modules/ordemServico/useCases/createOrdemPeca/CreateOrdemPecaController.ts
+++ src/modules/ordemServico/useCases/createOrdemPeca/CreateOrdemPecaController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { container } from "tsyringe";
-import { CreateOrdemPecaUseCase } from "./CreateOrdemServicoPecaUseCase";
+import { CreateOrdemPecaUseCase } from "./CreateOrdemPecaUseCase";
 
 class CreateOrdemPecaController {
     async handle(request: Request, response: Response): Promise<Response> {
