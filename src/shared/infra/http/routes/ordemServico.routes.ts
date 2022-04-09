5b37625c1240280b035777ee0b283e99a067c968
NathanFelipe99 <nathanfguerlando@gmail.com>
2022-04-09 09:32:04 -0300

    Adding pecas routes into ordemServico.routes

--- src/shared/infra/http/routes/ordemServico.routes.ts
+++ src/shared/infra/http/routes/ordemServico.routes.ts
@@ -3,6 +3,10 @@ import { Router } from "express";
 import { ListOrdemProcedimentosController } from "@modules/ordemServico/useCases/listOrdemProcedimentos/ListOrdemProcedimentosController";
 import { CreateOrdemProcedimentoController } from "@modules/ordemServico/useCases/createOrdemProcedimento/CreateOrdemProcedimentoController";
 
+import { ListOrdemPecasController } from "@modules/ordemServico/useCases/listOrdemPeca/ListOrdemPecasController";
+import { CreateOrdemPecaController } from "@modules/ordemServico/useCases/createOrdemPeca/CreateOrdemPecaController";
+import { DeleteOrdemPecaController } from "@modules/ordemServico/useCases/deleteOrdemPeca/DeleteOrdemPecaController";
+
 import { ListStatusOrdemController } from "@modules/ordemServico/useCases/listStatusOrdem/ListStatusOrdemController";
 
 import { ensureAuthenticated } from "../middlewares/ensureAuthentication";
@@ -18,10 +22,13 @@ ordemServicoRoutes.post("/procedimentos", ensureAuthenticated, createOrdemProced
 
 
 //Ordem Pecas
+const listOrdemPecasController = new ListOrdemPecasController();
+const createOrdemPecaController = new CreateOrdemPecaController();
+const deleteOrdemPecaController = new DeleteOrdemPecaController();
 
-
-
-
+ordemServicoRoutes.get("/pecas", ensureAuthenticated, listOrdemPecasController.handle)
+ordemServicoRoutes.post("/pecas", ensureAuthenticated, createOrdemPecaController.handle)
+ordemServicoRoutes.delete("/pecas/:id", ensureAuthenticated, deleteOrdemPecaController.handle)
 
 //Status Ordem
 const listStatusOrdemController = new ListStatusOrdemController();
