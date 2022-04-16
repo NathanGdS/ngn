e3f2f4dff48b6f79e5cd4cc783b4c3f7e8972e16
Nathan Garcia dos Santos <nathangar01@gmail.com>
2022-04-16 00:54:05 -0300

    Removendo as sujeiras de Telefone e Endereço

--- src/modules/cliente/useCases/deleteCliente/DeleteClienteUseCase.ts
+++ src/modules/cliente/useCases/deleteCliente/DeleteClienteUseCase.ts
@@ -1,6 +1,5 @@
 import { IAutomovelRepository } from "@modules/automovel/repositories/IAutomovelRepository";
 import { IClienteRepository } from "@modules/cliente/repositories/IClienteRepository";
-import { IEnderecoRepository } from "@modules/endereco/repositories/IEnderecoRepository";
 import { ITelefoneRepository } from "@modules/telefone/repositories/ITelefoneRepository";
 import { AppError } from "@shared/errors/AppError";
 import { inject, injectable } from "tsyringe";
@@ -14,24 +13,12 @@ class DeleteClienteUseCase {
         @inject("AutomovelRepository")
         private automovelRepository: IAutomovelRepository,
 
-        @inject("TelefoneRepository")
-        private telefoneRepository: ITelefoneRepository,
-
-        @inject("EnderecoRepository")
-        private enderecoRepository: IEnderecoRepository
     ) { }
     
     async execute(id: string): Promise<void> {
         
         const customerHasAutomobiles = await this.automovelRepository.findByCustomer(id)
         if (customerHasAutomobiles.length > 0) throw new AppError('Cliente tem Automóvel(is) cadastrado(s)! Exclua-o(s) antes de excluir este registro!')
-
-        const customerHasPhones = await this.telefoneRepository.findByCustomer(id)
-        if (customerHasPhones.length > 0) throw new AppError('Cliente tem telefone(s) cadastrado(s)! Exclua-o(s) antes de excluir este registro!')
-        
-        const customerHasAddresses = await this.enderecoRepository.findByCustomer(id)
-        if (customerHasAddresses) throw new AppError('Cliente tem endereço(s) cadastrado(s)! Exclua-o(s) antes de excluir este registro!')
-
         this.clienteRepository.delete(id)
     }
 }
--- src/shared/infra/http/routes/clientes.routes.ts
+++ src/shared/infra/http/routes/clientes.routes.ts
@@ -6,16 +6,6 @@ import { ListByIdClienteController } from "@modules/cliente/useCases/listByIdCli
 import { UpdateClienteController } from "@modules/cliente/useCases/updateCliente/UpdateClienteController";
 import { DeleteClienteController } from "@modules/cliente/useCases/deleteCliente/DeleteClienteController";
 
-import { CreateTelefoneController } from "@modules/telefone/useCases/createTelefone/CreateTelefoneController";
-import { ListTelefoneController } from "@modules/telefone/useCases/listTelefone/ListTelefoneController";
-import { UpdateTelefoneController } from "@modules/telefone/useCases/updateTelefone/UpdateTelefoneController";
-import { DeleteTelefoneController } from "@modules/telefone/useCases/deleteTelefone/DeleteTelefoneController";
-
-import { CreateEnderecoController } from "@modules/endereco/useCases/createEndereco/CreateEnderecoController";
-import { ListEnderecoController } from "@modules/endereco/useCases/listEndereco/ListEnderecoController";
-import { UpdateEnderecoController } from "@modules/endereco/useCases/updateEndereco/UpdateEnderecoController";
-import { DeleteEnderecoController } from "@modules/endereco/useCases/deleteEndereco/DeleteEnderecoController";
-
 import { ensureAuthenticated } from "../middlewares/ensureAuthentication";
 import { ensureAdmin } from "../middlewares/ensureAdmin";
 
@@ -27,30 +17,11 @@ const listByIdClienteController = new ListByIdClienteController();
 const updateClienteController = new UpdateClienteController();
 const deleteClienteController = new DeleteClienteController();
 
-const createTelefoneController = new CreateTelefoneController();
-const listTelefoneController = new ListTelefoneController();
-const updateTelefoneController = new UpdateTelefoneController();
-const deleteTelefoneController = new DeleteTelefoneController();
-
-const createEnderecoController = new CreateEnderecoController();
-const listEnderecoController = new ListEnderecoController();
-const updateEnderecoController = new UpdateEnderecoController();
-const deleteEnderecoController = new DeleteEnderecoController();
-
 clienteRoutes.post("/", ensureAuthenticated, createClienteController.handle);
 clienteRoutes.get("/", ensureAuthenticated, listClienteController.handle);
 clienteRoutes.get("/:id", ensureAuthenticated, listByIdClienteController.handle)
 clienteRoutes.put("/:id", ensureAuthenticated, updateClienteController.handle);
 clienteRoutes.delete("/:id", ensureAuthenticated, ensureAdmin, deleteClienteController.handle);
 
-clienteRoutes.post("/telefone", ensureAuthenticated, createTelefoneController.handle);
-clienteRoutes.get("/telefone", ensureAuthenticated, listTelefoneController.handle);
-clienteRoutes.put("/telefone/:id", ensureAuthenticated, updateTelefoneController.handle);
-clienteRoutes.delete("/telefone/:id", ensureAuthenticated, deleteTelefoneController.handle);
-
-clienteRoutes.post("/endereco", ensureAuthenticated, createEnderecoController.handle);
-clienteRoutes.get("/endereco", ensureAuthenticated, listEnderecoController.handle);
-clienteRoutes.put("/endereco/:id", ensureAuthenticated, updateEnderecoController.handle);
-clienteRoutes.delete("/endereco/:id", ensureAuthenticated, deleteEnderecoController.handle);
 
 export { clienteRoutes };
